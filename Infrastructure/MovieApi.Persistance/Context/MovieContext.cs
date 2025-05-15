fac4e08df226e23bfffa14d2523b009f87b85cee
Çağrı <cagrikandemir58@icloud.com>
2025-05-15 15:35:13 +0300

    Domain ve Persistence  Katmanı oluşturuldu ve Hazırlandı DbContext Bağlandı

---
+++ Infrastructure/MovieApi.Persistance/Context/MovieContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+
+namespace MovieApi.Persistance.Context;
+
+public class MovieContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=Cagri;initial Catalog=ApiMovieDb;integrated Security=true");
+    }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Cast> Casts { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+}
