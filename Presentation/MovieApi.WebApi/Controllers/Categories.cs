2dafb278233502a87fee235a8615dbb6da0e1bc3
Çağrı <cagrikandemir58@icloud.com>
2025-05-17 22:14:34 +0300

    ım started for Api controller but need fix for categories.cs

--- Presentation/MovieApi.WebApi/Controllers/Categories.cs
+++ Presentation/MovieApi.WebApi/Controllers/Categories.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.CategoryCommands;
+using MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers;
 
 namespace MovieApi.WebApi.Controllers
 {
@@ -7,5 +9,33 @@ namespace MovieApi.WebApi.Controllers
     [ApiController]
     public class Categories : ControllerBase
     {
+        private readonly CreateCategoryCommandHandler _createCategoryCommandHandler;
+        private readonly RemoveCategoryCommandHandler _removeCategoryCommandHandler;
+        private readonly UpdateCategoryCommandHandler _updateCategoryCommandHandler;
+        private readonly GetCategoryQueryHandler _getCategoriesQueryHandler;
+        private readonly GetCategoryByIdQueryHandler _getCategoryByIdQueryHandler;
+
+        public Categories(GetCategoryByIdQueryHandler getCategoryByIdQueryHandler, GetCategoryQueryHandler getCategoriesQueryHandler, UpdateCategoryCommandHandler updateCategoryCommandHandler, RemoveCategoryCommandHandler removeCategoryCommandHandler, CreateCategoryCommandHandler createCategoryCommandHandler)
+        {
+            _getCategoryByIdQueryHandler = getCategoryByIdQueryHandler;
+            _getCategoriesQueryHandler = getCategoriesQueryHandler;
+            _updateCategoryCommandHandler = updateCategoryCommandHandler;
+            _removeCategoryCommandHandler = removeCategoryCommandHandler;
+            _createCategoryCommandHandler = createCategoryCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _getCategoriesQueryHandler.Handle();
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCategoryCommand command)
+        {
+            var result = await _createCategoryCommandHandler.Handle(command);
+            return Ok("Kayıt Başarılı ! ");
+            //return Ok(await _createCategoryCommandHandler.Handle(command));
+        }
     }
 }
